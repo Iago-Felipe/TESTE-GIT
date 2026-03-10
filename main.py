9bcbee39e10dc6da74f3042e13a0541696c00d2e
Robson e Iago. <robson.novais@estudante.iftm.edu.br.>
2026-03-10 09:36:43 -0300

    FixBug: arrumando o erro no input

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-n = int (input('Digite um número ai bobão: '))
-print(n)
+x = True
+while x == True:
+    x = False
+    try:
+        n = int(input('Digite um número aí bobão: '))
+        print(n)
+    except ValueError:
+        print('Insira um numero pfv')
+        x = True
