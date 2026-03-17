af1a106d24d8adf586a5622dd96b3d7e243bd833
Iago Felipe <iagofelipe7667@gmail.com>
2026-03-17 09:25:43 -0300

    Atualização do Fatorial iterativo com adição do Fatorial recursivo

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+def Fatorial_rec(n):
+    if (n <= 1):
+        return 1
+    else: 
+        return n * Fatorial_rec(n - 1)
+
+def Fatorial(n):
+    for i in range(n-1, 0, -1):
+        n = n*i
+    return n
+
 x = True
 while x == True:
     x = False
     try:
         n = int(input('Digite um número aí bobão: '))
-        print(n)
+        print("")
+        print(f"Fatorial iterativo: {Fatorial(n)}")
+        print(f"Fatorial recursivo: {Fatorial_rec(n)}")
     except ValueError:
         print('Insira um numero pfv')
         x = True
-
-def Fatorial():
-    print("------- Fatorial -------------")
-    F = int(input("Digite o numero que deseja ver o fatorial: "))
-    for i in range(F-1, 0, -1):
-        F = F*i
-    print(F)
-
-Fatorial()
